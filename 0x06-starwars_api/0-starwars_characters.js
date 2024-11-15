5836c7b6cad040f1e77ddf04249fcd72bdfd841b
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-11-15 05:58:20 +0100

    Resolved issues

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,26 +6,37 @@
 
 const request = require('request');
 
-const filmEndpoint = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+if (process.argv.length < 3) {
+  console.log('Usage: ./0-starwars_characters.js <Movie.id>');
+  process.exit(1);
+}
 
-request(filmEndpoint, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
+const movieEndpoint = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-  if (response.statusCode === 200) {
-    body = JSON.parse(body);
-    for (const characterEndpoint of body.characters) {
-      request(characterEndpoint, (error, response, body) => {
-        if (error) {
-          console.error(error);
-          return;
-        }
+function fetchData (endpoint) {
+  return new Promise((resolve, reject) => {
+    request(endpoint, (error, response, body) => {
+      if (error) {
+        reject(error); // Reject the promise if there is an error;
+      } else if (response.statusCode !== 200) {
+        reject(
+          new Error(`Failed to load data from API, status code: ${response.statusCode}`)
+        );
+      }
+      resolve(JSON.parse(body));
+    });
+  });
+}
 
-        body = JSON.parse(body);
-        console.log(body.name);
-      });
+fetchData(movieEndpoint)
+  .then(data => {
+    const charactersData = data.characters.map(
+      characterEndpoint => fetchData(characterEndpoint));
+
+    return Promise.all(charactersData);
+  })
+  .then(charactersData => {
+    for (const character of charactersData) {
+      console.log(character.name);
     }
-  }
-});
+  });
